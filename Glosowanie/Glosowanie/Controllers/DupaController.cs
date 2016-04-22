dee8ac1b59b500ec74e2c518e135648bad3aae06
Eryk Siejka <eryk.siejka@hotmail.com>
2016-04-22 22:53:06 +0200

    added dupa controller

---
+++ Glosowanie/Glosowanie/Controllers/DupaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Glosowanie.Controllers
+{
+    public class DupaController : Controller
+    {
+        // GET: Dupa
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
