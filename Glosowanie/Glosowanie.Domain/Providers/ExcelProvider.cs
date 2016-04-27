3fbdb44d2fc0018cea64a13a599cdc2da518625e
Morson <eryk.siejka@hotmail.com>
2016-04-27 16:08:57 +0200

    Added logic

---
+++ Glosowanie/Glosowanie.Domain/Providers/ExcelProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GemBox.Spreadsheet;
+
+namespace Glosowanie.Domain.Providers
+{
+
+    public static class ExcelProvider
+    {
+        public static ExcelFile CreateExcelFile(IEnumerable<string> listaTokenow)
+        {
+            SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+
+            ExcelFile excelFile = new ExcelFile();
+            ExcelWorksheet excelWorksheet = excelFile.Worksheets.Add("Writing");
+
+            excelWorksheet.Cells[0, 0].Value = "Lista tokenów";
+            foreach (string token in listaTokenow)
+            {
+                excelWorksheet.Cells[token.GetEnumerator().Current, 0].Value = token;
+            }
+
+            return excelFile;
+        }
+    }
+}
