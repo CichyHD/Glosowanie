cc5240075cf8ec91e0e0dfe33d4047fdab42beb2
Morszyszka <Morszyszka@DESKTOP-94837NI>
2016-04-28 02:03:39 +0200

    rewtr

--- Glosowanie/Glosowanie/Controllers/DupaController.cs
+++ Glosowanie/Glosowanie/Controllers/DupaController.cs
@@ -8,10 +8,46 @@ namespace Glosowanie.Controllers
 {
     public class DupaController : Controller
     {
-        // GET: Dupa
-        public ActionResult Index()
+
+       
+        
+              [HttpGet]
+        public ActionResult SolvePool(Pool model)
         {
-            return View();
+
+            #region temporaryIn
+            string txt = "Czyje takie brzydkie dziecko?";
+
+            List<Answer> tempAns = new List<Answer>();
+
+            Answer a = new Answer()
+            {
+                AnswerText = "niczyje",
+                NumberOfChoses = 0
+            };
+
+            for (int i = 0; i < 3; i++)
+            {
+                tempAns.Add(a);
+
+            }
+            List<Question> questions = new List<Question>();
+            Question q = new Question() { QuestionText = txt, Answers = tempAns };
+            for (int i = 0; i < 10; i++)
+            {
+                questions.Add(q);
+            }
+            Pool model2 = new Pool()
+            {
+                Title = "tempTittle",
+                Questions = questions
+            };
+            #endregion
+
+
+            return View(model2);
         }
+            
+        
     }
 }
