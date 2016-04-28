4db81b77a15ed0019f0fd715854f3db6b3d506a2
CichyBrk <robert.borkowski.1994@gmail.com>
2016-04-28 02:07:57 +0200

    commit

--- Glosowanie/Glosowanie/Controllers/DupaController.cs
+++ Glosowanie/Glosowanie/Controllers/DupaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Glosowanie.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,7 @@ namespace Glosowanie.Controllers
     public class DupaController : Controller
     {
 
-       
-        
-              [HttpGet]
+       [HttpGet]
         public ActionResult SolvePool(Pool model)
         {
 
